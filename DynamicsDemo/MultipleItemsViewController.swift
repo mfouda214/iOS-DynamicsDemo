84724f5cc2f31a6dd5ac672cc93182eb3bb60997
mfouda214 <m.fouda@robone.net>
2018-08-01 23:18:07 +0100

    initial commit

---
+++ DynamicsDemo/MultipleItemsViewController.swift
@@ -0,0 +1,17 @@
+//
+//  MultipleItemsViewController.swift
+//  DynamicsDemo
+//
+//  Updated by Mohamed Sobhy Fouda on 8/31/18.
+//  Created by Hesham Abd-Elmegid on 6/23/16.
+//  Copyright © 2016 CareerFoundy. All rights reserved.
+//
+
+import UIKit
+
+class MultipleItemsViewController: UIViewController {
+
+    @IBAction func addViewButtonTapped(_ sender: AnyObject) {
+        
+    }
+}
---
+++ DynamicsDemo/SingleItemViewController.swift
@@ -0,0 +1,32 @@
+//
+//  SingleItemViewController.swift
+//  DynamicsDemo
+//
+//  Updated by Mohamed Sobhy Fouda on 8/31/18.
+//  Created by Hesham Abd-Elmegid on 6/23/16.
+//  Copyright © 2016 CareerFoundy. All rights reserved.
+//
+
+import UIKit
+
+class SingleItemViewController: UIViewController {
+    let blueSquareView = UIView()
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        addBlueSquareView()
+    }
+    
+    func addBlueSquareView() {
+        let frame = CGRect(x: 50, y: 70, width: 150, height: 150)
+        blueSquareView.frame = frame
+        blueSquareView.backgroundColor = .blue
+        view.addSubview(blueSquareView)
+    }
+
+    @IBAction func addBehaviorButtonTapped(_ sender: AnyObject) {
+        
+    }
+}
+
